d3e9f36619077d96106e659ac8a42428b20ff81f
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-05-19 13:29:02 +0300

    Cta flexible section

--- wp-content/themes/comfy/page.php
+++ wp-content/themes/comfy/page.php
@@ -4,15 +4,15 @@
  * @subpackage comfy
  */
 get_header();
-if (have_rows('flexible_sections')) :
-    $section_directory = 'template-parts/flexible-sections/';
+if ( have_rows( 'flexible_sections' ) ) :
+	$section_directory = 'template-parts/blocks/';
 
-    while (have_rows('flexible_sections')) :
-        the_row();
-        $section_name = str_replace('_', '-', get_row_layout());
-        get_template_part($section_directory . '/' . $section_name . '/' . $section_name);
-    endwhile;
-else:
-    the_content();
+	while ( have_rows( 'flexible_sections' ) ) :
+		the_row();
+		$section_name = str_replace( '_', '-', get_row_layout() );
+		get_template_part( $section_directory . '/' . $section_name . '/' . $section_name );
+	endwhile;
+else :
+	the_content();
 endif;
 get_footer();
