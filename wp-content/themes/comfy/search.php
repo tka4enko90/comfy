8a0e207d15f81683072fe598300987e30e0125e9
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-05-26 20:42:16 +0300

    Search & grid update

--- wp-content/themes/comfy/search.php
+++ wp-content/themes/comfy/search.php
@@ -9,20 +9,22 @@ if ( ! empty( $_GET['s'] ) ) :
 	$query = new WP_Query(
 		array(
 			'posts_per_page' => -1,
-			's'              => sanitize_text_field( $_GET['s'] ),
+			's'              => sanitize_text_field( $s ),
 		)
 	); ?>
 
 		<section class="section">
 			<div class="container">
 				<div class="row">
+					<div class="col-100">
+						<h1 class="archive-header-title"><?php echo __( 'Search results for: ', 'COMFY' ) . '"' . htmlentities( wp_kses( $s, array() ), ENT_QUOTES, 'UTF-8' ) . '"'; ?> </h1>
+					</div>
 					<?php
 					if ( $query->posts ) :
 						foreach ( $query->posts as $post ) :
 							$item['product'] = $post
 							?>
-
-							<div class="col">
+							<div class="col-100 col-md-50">
 								<?php echo  get_template_part( 'template-parts/product-preview', '', $item ); ?>
 							</div>
 							<?php
