54e0966351784a733eda043b0b2e91f1088ca70e
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-05-27 16:53:15 +0300

    thumbs & content-with-image fixes

--- wp-content/themes/comfy/includes/thumbs.php
+++ wp-content/themes/comfy/includes/thumbs.php
@@ -8,13 +8,13 @@ add_action(
 );
 
 if ( function_exists( 'add_image_size' ) ) {
-	add_image_size( 'cmf_fullwidth', 1360, 640, true );
+	add_image_size( 'cmf_fullwidth', 1360, 640 , true);
 	add_image_size( 'cmf_header_nav_image', 403, 256, true );
 	add_image_size( 'cmf_product_preview', 352, 352, true );
-	add_image_size( 'cmf_search_result', 54, 54, true );
-	add_image_size( 'cmf_logo', 91, 32, true );
-	add_image_size( 'cmf_footer_partner', 33, 20, true );
-	add_image_size( 'cmf_social_icon', 16, 16, true );
+	add_image_size( 'cmf_search_result', 54, 54 );
+	add_image_size( 'cmf_logo', 91, 32 );
+	add_image_size( 'cmf_footer_partner', 33, 20 );
+	add_image_size( 'cmf_social_icon', 16, 16 );
 	add_image_size( 'cmf_content_with_image_3', 490, 640, true );
 	add_image_size( 'cmf_content_with_image_1', 490, 577, true );
 	add_image_size( 'cmf_content_with_image_2', 664, 577, true );
