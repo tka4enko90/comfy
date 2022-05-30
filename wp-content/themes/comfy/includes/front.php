7bf7fa4e13b9320c091652a77811daebe8e12ca2
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-05-30 10:16:09 +0300

    Star rating fix

--- wp-content/themes/comfy/includes/front.php
+++ wp-content/themes/comfy/includes/front.php
@@ -4,5 +4,6 @@ add_action(
 	function () {
 		wp_enqueue_style( 'main', get_template_directory_uri() . '/dist/css/main.css', array(), 1.0, 'all' );
 		wp_enqueue_script( 'scripts', get_template_directory_uri() . '/dist/js/main.js', array( 'jquery' ), 1.0, true );
+		wp_enqueue_style( 'product-preview', get_template_directory_uri() . '/dist/css/partials/product-preview.css', '', '', 'all' );
 	}
 );
