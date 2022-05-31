1c47915aa8faf072ec1ad2900df9d3bcc200ca65
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-05-31 10:55:44 +0300

    Related posts & blog fixes

--- wp-content/themes/comfy/single.php
+++ wp-content/themes/comfy/single.php
@@ -76,6 +76,58 @@ $author      = array(
 			</div>
 		</div>
 	</article>
+		<?php
+		$orig_post = $post;
+		global $post;
+		$tags = wp_get_post_tags( $post->ID );
+
+		if ( ! empty( $tags ) ) {
+			?>
+			<section class="related-posts">
+				<div class="container">
+					<div class="row">
+						<div class="col-100">
+							<h3 class="related-posts-title"><?php _e( 'Related posts', 'comfy' ); ?></h3>
+						</div>
+
+						<?php
+						$tag_ids = array();
+						foreach ( $tags as $individual_tag ) {
+							$tag_ids[] = $individual_tag->term_id;
+						}
+						$args = array(
+							'tag__in'             => $tag_ids,
+							'post__not_in'        => array( $post->ID ),
+							'posts_per_page'      => 2, // Number of related posts to display.
+							'ignore_sticky_posts' => 1,
+						);
+
+						$my_query = new wp_query( $args );
+
+						while ( $my_query->have_posts() ) {
+							$my_query->the_post();
+							?>
+							<div class="col-100 col-sm-50">
+								<article class="post">
+									<?php the_post_thumbnail( 'cmf_review_slider' ); ?>
+									<?php get_template_part( 'template-parts/post', 'cats' ); ?>
+									<h4><?php the_title(); ?></h4>
+									<a class="button button-secondary" href="<?php the_permalink(); ?>">
+										<?php echo __( 'Read more', 'comfy' ); ?>
+									</a>
+								</article>
+							</div>
+							<?php
+						}
+						$post = $orig_post;
+						wp_reset_query();
+						?>
+					</div>
+				</div>
+			</section>
+			<?php
+		}
+		?>
 	<?php endwhile; ?>
 </main>
 <?php
