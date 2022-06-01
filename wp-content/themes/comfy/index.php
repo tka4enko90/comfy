5f4e1ca8ab6fb04158ae2235528b08b979797491
Alexander Tkachenko <sasha@markupus.tech>
2022-06-01 18:44:30 +0300

    feat: fixed layout blog posts

--- wp-content/themes/comfy/index.php
+++ wp-content/themes/comfy/index.php
@@ -18,12 +18,14 @@ get_header();
 		</div>
 	</section>
 	<?php
+    $i = 0;
 	if ( have_posts() ) :
 		?>
 	<section class="section archive-section">
 		<?php
 		while ( have_posts() ) :
 			the_post();
+		$i++;
 			$post_subtitle = get_field( 'post_subtitle' );
 			$excerpt       = ! empty( $post_subtitle ) ? $post_subtitle : get_the_excerpt();
 			?>
@@ -65,66 +67,70 @@ get_header();
 						<?php get_template_part( 'template-parts/blocks/content-with-image-advanced/content-with-image-advanced', '', $settings ); ?>
 					</div>
 					<div class="container container-sm">
-						<div class="row">
-							<div class="col col-100">
-								<h3 class="archive-section-title"><?php _e( 'Latest on our blog', 'comfy' ); ?></h3>
-							</div>
-					<?php
-					break;
-				case 4:
-					?>
-							<div class="col-100 col-sm-50 post-col">
-								<?php get_template_part( 'template-parts/post', 'preview' ); ?>
-							</div>
-						</div>
-					</div>
-					<?php
-					break;
-				case 5:
-					?>
-					<div class="container">
-						<div class="row">
-							<div class="col-100">
-								<article class="post post-big">
-									<div class="post-thumb">
-										<?php the_post_thumbnail( 'cmf_post_big' ); ?>
-									</div>
-									<div class="post-content">
-										<?php get_template_part( 'template-parts/post', 'cats' ); ?>
-										<h2 class="post-title-big">
-											<?php the_title(); ?>
-										</h2>
-										<?php
-										if ( ! empty( $excerpt ) ) {
-											?>
-											<p>
-												<?php echo $excerpt; ?>
-											</p>
-											<?php
-										}
-										?>
-										<a class="button button-secondary-white" href="<?php get_permalink(); ?>">
-											<?php echo $post_button_label; ?>
-										</a>
-									</div>
-								</article>
-							</div>
-						</div>
-					</div>
-					<div class="container container-sm">
+                        <h3 class="archive-section-title"><?php _e( 'Latest on our blog', 'comfy' ); ?></h3>
 						<div class="row">
 					<?php
 					break;
+//				case 4:
+//					?>
+<!--							<div class="col-100 col-sm-50 post-col">-->
+<!--								--><?php //get_template_part( 'template-parts/post', 'preview' ); ?>
+<!--							</div>-->
+<!--						</div>-->
+<!--					</div>-->
+<!--					--><?php
+//					break;
+//				case 5:
+//					?>
+<!--					<div class="container">-->
+<!--						<div class="row">-->
+<!--							<div class="col-100">-->
+<!--								<article class="post post-big">-->
+<!--									<div class="post-thumb">-->
+<!--										--><?php //the_post_thumbnail( 'cmf_post_big' ); ?>
+<!--									</div>-->
+<!--									<div class="post-content">-->
+<!--										--><?php //get_template_part( 'template-parts/post', 'cats' ); ?>
+<!--										<h2 class="post-title-big">-->
+<!--											--><?php //the_title(); ?>
+<!--										</h2>-->
+<!--										--><?php
+//										if ( ! empty( $excerpt ) ) {
+//											?>
+<!--											<p>-->
+<!--												--><?php //echo $excerpt; ?>
+<!--											</p>-->
+<!--											--><?php
+//										}
+//										?>
+<!--										<a class="button button-secondary-white" href="--><?php //get_permalink(); ?><!--">-->
+<!--											--><?php //echo $post_button_label; ?>
+<!--										</a>-->
+<!--									</div>-->
+<!--								</article>-->
+<!--							</div>-->
+<!--						</div>-->
+<!--					</div>-->
+<!--					<div class="container container-sm">-->
+<!--						<div class="row">-->
+<!--					--><?php
+//					break;
 				default:
-					?>
-					<div class="col-100 col-sm-50 post-col">
-						<?php get_template_part( 'template-parts/post', 'preview' ); ?>
-					</div>
-					<?php
+				    $args = array();
+                    if ($i == 6) {
+                        $args['image_size'] = 'cmf_post_big';
+                        $args['class'] = 'cmf_post_big';
+                    };
+                    ?>
+                    <div class="post-col <?php echo !empty($args['class']) ? 'post-big' : '' ; ?>">
+                        <?php get_template_part( 'template-parts/post', 'preview', $args ); ?>
+                    </div>
+                    <?php
+                    break;
 					break;
 			}
 				endwhile;
-		?>
+		        ?>
 				<div class="col-100">
 						<?php get_template_part( 'template-parts/pagination' ); ?>
 				</div>
