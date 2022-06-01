8ed97f241fab5ed2d8674d5308cfa778f4dcb109
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-06-01 13:03:03 +0300

    Blog adaptive update

--- wp-content/themes/comfy/single.php
+++ wp-content/themes/comfy/single.php
@@ -23,23 +23,19 @@ $author      = array(
 		?>
 	<article class="post">
 		<div class="container container-xs">
-			<div class="row">
-				<div class="col col-100">
-					<div class="post-heading">
-						<?php get_template_part( 'template-parts/post', 'cats' ); ?>
-						<h1 class="post-title"><?php the_title(); ?></h1>
-						<?php
-						if ( ! empty( $post_fields['subtitle'] ) ) {
-							?>
-							<p class="post-subtitle">
-								<?php echo $post_fields['subtitle']; ?>
-							</p>
-							<?php
-						}
-						?>
-						<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished"><?php echo get_the_date(); ?></time>
-					</div>
-				</div>
+			<div class="post-heading">
+				<?php get_template_part( 'template-parts/post', 'cats' ); ?>
+				<h1 class="post-title"><?php the_title(); ?></h1>
+				<?php
+				if ( ! empty( $post_fields['subtitle'] ) ) {
+					?>
+					<p class="post-subtitle">
+						<?php echo $post_fields['subtitle']; ?>
+					</p>
+					<?php
+				}
+				?>
+				<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished"><?php echo get_the_date(); ?></time>
 			</div>
 		</div>
 		<div class="container container-lg">
@@ -48,32 +44,26 @@ $author      = array(
 			</div>
 		</div>
 		<div class="container container-xs">
-			<div class="row">
-				<div class="col-100">
-					<div class="post-content">
-						<?php the_content(); ?>
-					</div>
-				</div>
-				<div class="col-100">
-					<figure class="post-author">
-						<?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
-						<figcaption class="post-author-info">
-							<?php
-							if ( ! empty( $author['name'] ) ) {
-								?>
-								<h5 class="post-author-name"><?php echo $author['name']; ?></h5>
-								<?php
-							}
-							if ( ! empty( $author['info'] ) ) {
-								?>
-								<p><?php echo $author['info']; ?></p>
-								<?php
-							}
-							?>
-						</figcaption>
-					</figure>
-				</div>
+			<div class="post-content">
+				<?php the_content(); ?>
 			</div>
+			<figure class="post-author">
+				<?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
+				<figcaption class="post-author-info">
+					<?php
+					if ( ! empty( $author['name'] ) ) {
+						?>
+						<h5 class="post-author-name"><?php echo $author['name']; ?></h5>
+						<?php
+					}
+					if ( ! empty( $author['info'] ) ) {
+						?>
+						<p><?php echo $author['info']; ?></p>
+						<?php
+					}
+					?>
+				</figcaption>
+			</figure>
 		</div>
 	</article>
 		<?php
