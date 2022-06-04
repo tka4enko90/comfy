9c2b33946884315c37256427b7183fa8aeb6bfe5
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-06-04 19:12:40 +0300

    Desktop changes

--- wp-content/themes/comfy/index.php
+++ wp-content/themes/comfy/index.php
@@ -14,127 +14,80 @@ get_header();
 <main class="main first-section-margin-64px">
 	<section class="section">
 		<div class="container container-sm">
-            <h1 class="page-title"><?php echo $page_title; ?></h1>
+			<h1 class="page-title"><?php echo $page_title; ?></h1>
 		</div>
 	</section>
 	<?php
-    $i = 0;
+	$i = 0;
 	if ( have_posts() ) :
 		?>
 	<section class="section archive-section">
-		<?php
-		while ( have_posts() ) :
-			the_post();
-		$i++;
-			$post_subtitle = get_field( 'post_subtitle' );
-			$excerpt       = ! empty( $post_subtitle ) ? $post_subtitle : get_the_excerpt();
-			?>
+		<div class="container container-sm">
+			<div class="row">
 			<?php
-			switch ( $post_counter++ ) {
-				case 0:
-					$settings = array(
-						'image_position' => 'left',
-						'content_width'  => '43', // %
-						'image_group'    => array(
-							'image_id' => get_post_thumbnail_id(),
-							'size'     => 'cmf_review_slider',
-						),
-					);
-					$settings[ 'content_padding_' . $settings['image_position'] ] = '18'; // px
-					ob_start();
-					get_template_part( 'template-parts/post', 'cats' );
+			while ( have_posts() ) :
+				the_post();
+
+				$post_subtitle = get_field( 'post_subtitle' );
+				$excerpt       = ! empty( $post_subtitle ) ? $post_subtitle : get_the_excerpt();
+				?>
+				<?php
+				$i++;
+				$args = array();
+				if ( 1 === $i ) {
+					$args['image_size']       = 'cmf_review_slider';
+					$args['class']            = 'post-first';
+					$args['content_wrap']     = true;
+					$args['btn_with_content'] = true;
+					$args['excerpt']          = $excerpt;
+				}
+				if ( 2 === $i ) {
 					?>
-					<h2><?php the_title(); ?></h2>
+					<h3 class="archive-section-title"><?php echo __( 'Latest on our blog', 'comfy' ); ?></h3>
 					<?php
-					if ( ! empty( $excerpt ) ) {
+				}
+
+				if ( 6 === $i ) {
+					$args['image_size'] = 'cmf_post_big';
+					$args['class']      = 'post-big';
+					$args['excerpt']    = $excerpt;
+				}
+				/*
+				switch ( $i++ ) {
+					case 0:
+						$args = array(
+							'image_size'       => 'cmf_review_slider',
+							'class'            => 'post-first',
+							'content_wrap'     => true,
+							'btn_with_content' => true,
+							'excerpt'          => $excerpt,
+
+						);
+						break;
+					case 1:
 						?>
-						<p class="short-description">
-						<?php echo $excerpt; ?>
-						</p>
+						<h3 class="archive-section-title"><?php echo __( 'Latest on our blog', 'comfy' ); ?></h3>
 						<?php
-					}
-					?>
+						break;
+					case 5:
+						$args = array(
+							'image_size' => 'cmf_post_big',
+							'class'      => 'post-big',
+							'excerpt'    => $excerpt,
+						);
+						break;
 
-					<?php
-					$settings['content_group']['content'] = ob_get_clean();
-					$settings['content_group']['link']    = array(
-						'url'   => get_permalink(),
-						'title' => $post_button_label,
-
-					);
-					?>
-					<div class="content-with-image-advanced-section">
-						<?php get_template_part( 'template-parts/blocks/content-with-image-advanced/content-with-image-advanced', '', $settings ); ?>
-					</div>
-					<div class="container container-sm">
-                        <h3 class="archive-section-title"><?php _e( 'Latest on our blog', 'comfy' ); ?></h3>
-						<div class="row">
-					<?php
-					break;
-//				case 4:
-//					?>
-<!--							<div class="col-100 col-sm-50 post-col">-->
-<!--								--><?php //get_template_part( 'template-parts/post', 'preview' ); ?>
-<!--							</div>-->
-<!--						</div>-->
-<!--					</div>-->
-<!--					--><?php
-//					break;
-//				case 5:
-//					?>
-<!--					<div class="container">-->
-<!--						<div class="row">-->
-<!--							<div class="col-100">-->
-<!--								<article class="post post-big">-->
-<!--									<div class="post-thumb">-->
-<!--										--><?php //the_post_thumbnail( 'cmf_post_big' ); ?>
-<!--									</div>-->
-<!--									<div class="post-content">-->
-<!--										--><?php //get_template_part( 'template-parts/post', 'cats' ); ?>
-<!--										<h2 class="post-title-big">-->
-<!--											--><?php //the_title(); ?>
-<!--										</h2>-->
-<!--										--><?php
-//										if ( ! empty( $excerpt ) ) {
-//											?>
-<!--											<p>-->
-<!--												--><?php //echo $excerpt; ?>
-<!--											</p>-->
-<!--											--><?php
-//										}
-//										?>
-<!--										<a class="button button-secondary-white" href="--><?php //get_permalink(); ?><!--">-->
-<!--											--><?php //echo $post_button_label; ?>
-<!--										</a>-->
-<!--									</div>-->
-<!--								</article>-->
-<!--							</div>-->
-<!--						</div>-->
-<!--					</div>-->
-<!--					<div class="container container-sm">-->
-<!--						<div class="row">-->
-<!--					--><?php
-//					break;
-				default:
-				    $args = array();
-                    if ($i == 6) {
-                        $args['image_size'] = 'cmf_post_big';
-                        $args['class'] = 'cmf_post_big';
-                    };
-                    ?>
-                    <div class="post-col <?php echo !empty($args['class']) ? 'post-big' : '' ; ?>">
-                        <?php get_template_part( 'template-parts/post', 'preview', $args ); ?>
-                    </div>
-                    <?php
-                    break;
-					break;
-			}
-				endwhile;
-		        ?>
-				<div class="col-100">
-						<?php get_template_part( 'template-parts/pagination' ); ?>
+				}
+				*/
+				?>
+				<div class="post-col <?php echo ! empty( $args['class'] ) ? $args['class'] : ''; ?>">
+					<?php get_template_part( 'template-parts/post', 'preview', $args ); ?>
 				</div>
+				<?php
+			endwhile;
+			?>
 			</div>
+			<?php get_template_part( 'template-parts/pagination' ); ?>
 		</div>
 	</section>
 		<?php
--- wp-content/themes/comfy/template-parts/post-preview.php
+++ wp-content/themes/comfy/template-parts/post-preview.php
@@ -1,22 +1,62 @@
 <?php /* Post preview */
 wp_enqueue_style( 'post-preview', get_template_directory_uri() . '/dist/css/partials/post-preview.css', '', '', 'all' );
-$button_label = ( ! empty( $args['button_label'] ) ) ? $args['button_label'] : __( 'Read more', 'comfy' );
-$image_size = !empty($args['image_size']) ?? 'cmf_post_preview';
+$btn_with_content = ! empty( $args['btn_with_content'] ) ? true : false;
+$button_label     = ( ! empty( $args['button_label'] ) ) ? $args['button_label'] : __( 'Read more', 'comfy' );
+$image_size       = ! empty( $args['image_size'] ) ? $args['image_size'] : 'cmf_post_preview';
+$excerpt          = ! empty( $args['excerpt'] ) ? $args['excerpt'] : '';
 ?>
 <article class="post">
+	<?php
+	if ( false === $btn_with_content ) {
+		?>
 	<div class="post-wrap">
+		<?php
+	}
+	?>
+
 		<div class="post-thumb">
 			<?php the_post_thumbnail( $image_size ); ?>
 		</div>
-		<?php get_template_part( 'template-parts/post', 'cats' ); ?>
-		<h3 class="post-title"><?php the_title(); ?></h3>
-        <a class="button button-secondary" href="<?php get_permalink(); ?>">
-            <?php echo $button_label; ?>
-        </a>
+		<?php
+		if ( isset( $args['content_wrap'] ) && true === $args['content_wrap'] ) {
+			?>
+		<div class="post-info">
+			<?php
+		}
+		?>
+			<?php get_template_part( 'template-parts/post', 'cats' ); ?>
+			<h3 class="post-title"><?php the_title(); ?></h3>
+			<?php
+			if ( ! empty( $excerpt ) ) {
+				?>
+				<p>
+					<?php echo $excerpt; ?>
+				</p>
+				<?php
+			}
+			if ( true === $btn_with_content ) {
+				?>
+				<a class="button button-secondary" href="<?php echo get_permalink(); ?>">
+					<?php echo $button_label; ?>
+				</a>
+				<?php
+			}
+			?>
+		<?php
+		if ( isset( $args['content_wrap'] ) && true === $args['content_wrap'] ) {
+			?>
+		</div>
+				<?php
+		}
+		if ( false === $btn_with_content ) {
+			?>
 	</div>
-
-	<?php // the_content(); ?>
-	<?php //wp_link_pages(); ?>
+	<a class="button button-secondary" href="<?php echo get_permalink(); ?>">
+			<?php echo $button_label; ?>
+	</a>
+				<?php
+		}
+		?>
 	<?php edit_post_link(); ?>
 </article>
 
