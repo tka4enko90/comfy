e589ad434c18723fbc36c77ea8a37aa95443e826
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-06-07 10:12:06 +0300

    “Working with Comfy..." section

--- wp-content/themes/comfy/template-parts/blocks/content-with-image--hello-we-re-comfy/content-with-image--hello-we-re-comfy.php
+++ wp-content/themes/comfy/template-parts/blocks/content-with-image--hello-we-re-comfy/content-with-image--hello-we-re-comfy.php
@@ -5,7 +5,6 @@ if ( ! empty( $args['section_name'] ) ) {
 
 $image_col = get_sub_field( 'image' );
 $settings  = array(
-	'container'    => 'sm',
 	'image_group'  => array(
 		'image_id' => $image_col['image_id'],
 		'size'     => 'cmf_content_with_image_1',
