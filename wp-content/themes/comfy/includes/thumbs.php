bf214d06d2dc3e243aa3e3480b6aac433de7d783
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-06-09 12:36:43 +0300

    thumbs rollback

--- wp-content/themes/comfy/includes/thumbs.php
+++ wp-content/themes/comfy/includes/thumbs.php
@@ -8,10 +8,10 @@ add_action(
 );
 
 if ( function_exists( 'add_image_size' ) ) {
-	add_image_size( 'cmf_fullwidth', 1360 );
-	add_image_size( 'cmf_post_single', 1360 );
-	add_image_size( 'cmf_post_big', 1186 );
-	add_image_size( 'cmf_header_nav_image', 403 );
+	add_image_size( 'cmf_fullwidth', 1360, 640, true );
+	add_image_size( 'cmf_post_single', 1360, 598, true );
+	add_image_size( 'cmf_post_big', 1186, 480, true );
+	add_image_size( 'cmf_header_nav_image', 403, 256, true );
 	add_image_size( 'cmf_post_preview', 403, 403, true );
 	add_image_size( 'cmf_product_preview', 352, 352, true );
 
@@ -22,11 +22,11 @@ if ( function_exists( 'add_image_size' ) ) {
 	add_image_size( 'cmf_social_icon', 16, 16 );
 	add_image_size( 'cmf_sign', 226, 83 );
 
-	add_image_size( 'cmf_content_with_image_3', 490 );
-	add_image_size( 'cmf_content_with_image_1', 490 );
-	add_image_size( 'cmf_content_with_image_2', 664 );
-	add_image_size( 'cmf_review_slider', 577 );
-	add_image_size( 'cmf_benefits_section', 402 );
-	add_image_size( 'cmf_contact_form', 577 );
+	add_image_size( 'cmf_content_with_image_3', 490, 640, true );
+	add_image_size( 'cmf_content_with_image_1', 490, 577, true );
+	add_image_size( 'cmf_content_with_image_2', 664, 577, true );
+	add_image_size( 'cmf_review_slider', 577, 577, true );
+	add_image_size( 'cmf_benefits_section', 402, 525, true );
+	add_image_size( 'cmf_contact_form', 577, 840, true );
 
 }
