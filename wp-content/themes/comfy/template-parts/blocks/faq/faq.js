5e3d1536533d1594c929ef6def9aab9d032f6803
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-06-10 13:14:20 +0300

    Script refactoring

--- wp-content/themes/comfy/template-parts/blocks/faq/faq.js
+++ wp-content/themes/comfy/template-parts/blocks/faq/faq.js
@@ -1,17 +1,12 @@
 (function ($) {
 	const slideSpeed = 600;
-	$( document ).on(
-		'ready',
-		function () {
-			$( '.faq-item-title' ).on(
-				'click',
-				function() {
-					if ( ! $( this ).hasClass( 'active' )) {
-						$( '.faq-item-title.active' ).removeClass( 'active' ).parent( '.faq-item' ).find( '.faq-item-content' ).slideToggle( slideSpeed );
-					}
-					$( this ).toggleClass( 'active' ).parent( '.faq-item' ).find( '.faq-item-content' ).slideToggle( slideSpeed );
-				}
-			);
+	$( '.faq-item-title' ).on(
+		'click',
+		function() {
+			if ( ! $( this ).hasClass( 'active' )) {
+				$( '.faq-item-title.active' ).removeClass( 'active' ).parent( '.faq-item' ).find( '.faq-item-content' ).slideToggle( slideSpeed );
+			}
+			$( this ).toggleClass( 'active' ).parent( '.faq-item' ).find( '.faq-item-content' ).slideToggle( slideSpeed );
 		}
 	);
 })( jQuery );
