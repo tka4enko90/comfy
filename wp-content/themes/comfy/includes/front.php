8f5dda3893098d74ec0ddf26200eecdaf47361e5
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-06-15 18:23:50 +0300

    Disable WordPress editor for default page template

--- wp-content/themes/comfy/includes/front.php
+++ wp-content/themes/comfy/includes/front.php
@@ -2,9 +2,8 @@
 add_action(
 	'wp_enqueue_scripts',
 	function () {
-        wp_dequeue_style( 'wp-block-library' );
-        wp_dequeue_style( 'wp-block-library-theme' ); // Wordpress core
-        //wp_dequeue_style( 'wc-block-style' ); // WooCommerce
+		wp_dequeue_style( 'wp-block-library' );
+		wp_dequeue_style( 'wp-block-library-theme' ); // WordPress core
 
 		wp_enqueue_style( 'main', get_template_directory_uri() . '/dist/css/main.css', array(), 1.0, 'all' );
 		wp_enqueue_script( 'scripts', get_template_directory_uri() . '/dist/js/main.js', array( 'jquery' ), 1.0, true );
@@ -13,8 +12,7 @@ add_action(
 );
 
 
-remove_action('wp_head', 'print_emoji_detection_script', 7);// REMOVE EMOJI ICONS Script
-remove_action('wp_print_styles', 'print_emoji_styles');     // REMOVE EMOJI ICONS Style
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );// REMOVE EMOJI ICONS Script
+remove_action( 'wp_print_styles', 'print_emoji_styles' );     // REMOVE EMOJI ICONS Style
 remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
 remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
-add_filter('use_block_editor_for_post_type', '__return_false', 10);// Disable Gutenberg editor.
