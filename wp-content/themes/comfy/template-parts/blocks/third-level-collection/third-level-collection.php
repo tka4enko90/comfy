a50989db52d01d2fe7232f9da2b6e02960550a86
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-07-07 08:35:26 +0300

    Third Level Collection Section added - https://markupus.flowfast.io/space/32418

---
+++ wp-content/themes/comfy/template-parts/blocks/third-level-collection/third-level-collection.php
@@ -0,0 +1,52 @@
+<?php
+if ( ! empty( $args['section_name'] ) ) {
+	wp_enqueue_style( $args['section_name'] . '-section', get_template_directory_uri() . '/template-parts/blocks/' . $args['section_name'] . '/' . $args['section_name'] . '.css', '', '', 'all' );
+}
+$section = array(
+	'title'    => get_sub_field( 'title' ),
+	'subtitle' => get_sub_field( 'subtitle' ),
+	'products' => get_sub_field( 'products' ),
+);
+?>
+<div class="container container-sm">
+	<?php
+	if ( ! empty( $section['title'] ) ) {
+		?>
+		<h2 class="section-title">
+			<?php echo $section['title']; ?>
+		</h2>
+		<?php
+	}
+	if ( ! empty( $section['subtitle'] ) ) {
+		?>
+		<p class="section-subtitle">
+			<?php echo $section['subtitle']; ?>
+		</p>
+		<?php
+	}
+	if ( ! empty( $section['products'] ) ) {
+		?>
+		<div class="row">
+			<?php
+			$product_args = array(
+				'thumb'            => 'cmf_post_preview', // optional
+				'show_cats'        => false,
+				'show_stars'       => true,
+				'show_reviews_num' => true,
+			);
+			foreach ( $section['products'] as $item ) {
+				if ( is_object( $item['product'] ) ) {
+					$product_args['product'] = $item['product'];
+					?>
+					<div class="col">
+						<?php get_template_part( 'template-parts/product-preview', '', $product_args ); ?>
+					</div>
+					<?php
+				}
+			}
+			?>
+		</div>
+		<?php
+	}
+	?>
+</div>
