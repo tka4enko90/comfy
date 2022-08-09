bcbe501f0274b0141a06b695474aebd174fa0e82
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-08-09 14:15:04 +0300

    Search results changes

--- wp-content/themes/comfy/search.php
+++ wp-content/themes/comfy/search.php
@@ -4,6 +4,8 @@
  * @subpackage comfy
  */
 
+wp_enqueue_style( 'search-results', get_template_directory_uri() . '/dist/css/pages/search-results.css', '', '', 'all' );
+
 get_header();
 if ( ! empty( $_GET['s'] ) ) :
 	$query = new WP_Query(
@@ -12,29 +14,34 @@ if ( ! empty( $_GET['s'] ) ) :
 			's'              => sanitize_text_field( $s ),
 		)
 	); ?>
-
-		<section class="section">
-			<div class="container">
-				<div class="row">
-					<div class="col-100">
-						<h1 class="archive-header-title"><?php echo __( 'Search results for: ', 'COMFY' ) . '"' . htmlentities( wp_kses( $s, array() ), ENT_QUOTES, 'UTF-8' ) . '"'; ?> </h1>
-					</div>
-					<?php
-					if ( $query->posts ) :
-						foreach ( $query->posts as $post ) :
-							$item['product'] = $post
-							?>
-							<div class="col-100 col-md-50">
-								<?php echo  get_template_part( 'template-parts/product-preview', '', $item ); ?>
-							</div>
-							<?php
-						endforeach;
-					endif;
-					?>
+<main class="main">
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-100">
+					<h3 class="archive-header-title">
+						<?php echo __( 'Search results for: ', 'COMFY' ) . '"' . htmlentities( wp_kses( $s, array() ), ENT_QUOTES, 'UTF-8' ) . '"'; ?>
+					</h3>
+					<p class="archive-header-subtitle">
+						<?php echo count( $query->posts ) . ' ' . __( 'results found', 'comfy' ); ?>
+					</p>
 				</div>
+				<?php
+				if ( $query->posts ) :
+					foreach ( $query->posts as $post ) :
+						$item['product'] = $post
+						?>
+						<div class="col-100 col-md-50">
+							<?php echo  get_template_part( 'template-parts/product-preview', '', $item ); ?>
+						</div>
+						<?php
+					endforeach;
+				endif;
+				?>
 			</div>
-		</section>
-
+		</div>
+	</section>
+</main>
 	<?php
 	wp_reset_query();
 endif;
