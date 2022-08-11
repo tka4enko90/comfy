dd65c7a84494d29231a70191ac1d4293edea24df
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-08-11 21:33:08 +0300

    Search Archive fixes

--- wp-content/themes/comfy/template-parts/product-preview.php
+++ wp-content/themes/comfy/template-parts/product-preview.php
@@ -6,22 +6,23 @@ if ( isset( $args ) && isset( $args['product'] ) && isset( $args['product']->ID 
 	if ( ! isset( $args['thumb'] ) ) {
 		$args['thumb'] = 'cmf_product_preview';
 	}
-	?>
-	<article class="product">
-		<a href="<?php echo $product->get_permalink(); ?>" class="product-link">
-			<div class="product-image-wrap">
-				<?php
-				if ( ! empty( $args['label'] ) ) {
-					?>
-					<div class="product-labels">
-						<?php
-						switch ( $args['label'] ) {
-							case 'tags':
-							case 'tag':
-								$terms = wp_get_post_terms( $product->get_id(), 'product_tag' );
-								if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
-									foreach ( $terms as $term ) {
-										?>
+	if ( ! empty( $product ) ) {
+		?>
+		<article class="product">
+			<a href="<?php echo $product->get_permalink(); ?>" class="product-link">
+				<div class="product-image-wrap">
+					<?php
+					if ( ! empty( $args['label'] ) ) {
+						?>
+						<div class="product-labels">
+							<?php
+							switch ( $args['label'] ) {
+								case 'tags':
+								case 'tag':
+									$terms = wp_get_post_terms( $product->get_id(), 'product_tag' );
+									if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
+										foreach ( $terms as $term ) {
+											?>
 											<span class="product-label">
 											<?php echo $term->name; ?>
 											</span>
@@ -29,47 +30,48 @@ if ( isset( $args ) && isset( $args['product'] ) && isset( $args['product']->ID 
 											if ( 'tag' === $args['label'] ) {
 												break 2;
 											}
+										}
 									}
-								}
-								break;
-							case 'cat':
-								?>
+									break;
+								case 'cat':
+									?>
 									<span class="product-label">
-								<?php
-								$term = get_term_by( 'id', $args['term_id'], 'product_cat' );
-								echo $term->name;
-								?>
+									<?php
+									$term = get_term_by( 'id', $args['term_id'], 'product_cat' );
+									echo $term->name;
+									?>
 									</span>
-								<?php
-								break;
-							case 'cats':
-								$terms = get_the_terms( $args['product']->ID, 'product_cat' );
-								if ( is_array( $terms ) && 0 < count( $terms ) ) {
-									foreach ( $terms as $term ) {
-										?>
+									<?php
+									break;
+								case 'cats':
+									$terms = get_the_terms( $args['product']->ID, 'product_cat' );
+									if ( is_array( $terms ) && 0 < count( $terms ) ) {
+										foreach ( $terms as $term ) {
+											?>
 											<span class="product-label">
 											<?php echo $term->name; ?>
 											</span>
 											<?php
+										}
 									}
-								}
-								break;
-						}
-						?>
-					</div>
-					<?php
-				}
-				echo get_the_post_thumbnail( $args['product']->ID, $args['thumb'] );
-				?>
-			</div>
-			<div class="product-info">
-				<h5 class="product-title"><?php echo get_the_title( $args['product'] ); ?></h5>
-				<div class="product-other-info">
-					<?php do_action( 'woocommerce_after_shop_loop_item_title' ); ?>
+									break;
+							}
+							?>
+						</div>
+						<?php
+					}
+					echo get_the_post_thumbnail( $args['product']->ID, $args['thumb'] );
+					?>
 				</div>
-			</div>
-		</a>
-	</article>
-	<?php
+				<div class="product-info">
+					<h5 class="product-title"><?php echo get_the_title( $args['product'] ); ?></h5>
+					<div class="product-other-info">
+						<?php do_action( 'woocommerce_after_shop_loop_item_title' ); ?>
+					</div>
+				</div>
+			</a>
+		</article>
+		<?php
+	}
 }
 ?>
