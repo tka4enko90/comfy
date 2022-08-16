9202f104a8ebbfff5bad3a4e5a68a53947334cee
Artem Eryzhenko <aeryzhenko@markupus.tech>
2022-08-16 16:27:19 +0300

    Disables the block editor from managing widgets

--- wp-content/themes/comfy/includes/etc/editor.php
+++ wp-content/themes/comfy/includes/etc/editor.php
@@ -1,6 +1,10 @@
 <?php
 add_filter( 'use_block_editor_for_post_type', '__return_false', 10 );// Disable Gutenberg editor.
 
+// Disables the block editor from managing widgets in the Gutenberg plugin.
+add_filter( 'gutenberg_use_widgets_block_editor', '__return_false' );
+// Disables the block editor from managing widgets.
+add_filter( 'use_widgets_block_editor', '__return_false' );
 
 add_action(
 	'init',
